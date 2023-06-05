a282be2ce0fdfa96ed60d59cced8f76c2be57ec5
udaykumarkasha <udayrk19@gmail.com>
2023-06-05 16:05:08 -0500

    Interfaces and constructors

--- src/June02/myFinalClass.java
+++ src/June02/myFinalClass.java
@@ -1,10 +1,10 @@
 package June02;
 
-public final class myFinalClass {
+final class myFinalClass {
 
-    protected final String family = "Kasha";
+    final String family = "Kasha";
 
-    protected final void displayFamily(){
+    final void displayFamily(){
         System.out.println("The family name is " + family);
     }
 }
