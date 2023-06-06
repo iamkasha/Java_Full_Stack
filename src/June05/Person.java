d338d93074f037c03a18e9052fc9734a8259d4df
udaykumarkasha <udayrk19@gmail.com>
2023-06-05 22:10:34 -0500

    Conditional Statements and loops

---
+++ src/June05/Person.java
@@ -0,0 +1,14 @@
+package June05;
+
+public class Person {
+    public void ageGroup(int age){
+
+        if(age >= 13 && age <= 19){
+            System.out.println("The person is a teen");
+        } else if(age < 13){
+            System.out.println("The person is a kid");
+        } else if(age > 19){
+            System.out.println("The person is an adult");
+        }
+    }
+}
