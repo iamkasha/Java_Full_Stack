3955a73fedd52875dbdf79de8451eb2d300bd79e
udaykumarkasha <udayrk19@gmail.com>
2023-06-09 16:13:25 -0500

    CSV program

--- src/June07/Student.java
+++ src/June07/Student.java
@@ -1,30 +1,23 @@
 package June07;
 
-public class Student {
-    private String firstName;
-    private String lastName;
-    private String gpa;
+public class Student implements Comparable<Student>{
+    public String firstName;
+    public String lastName;
+    public float gpa;
 
-    public Student(String firstName, String lastName, String gpa) {
+    public Student(String firstName, String lastName, float gpa) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gpa = gpa;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getGpa() {
-        return gpa;
-    }
 
     public int compareTo(Student other){
         return this.lastName.compareTo(other.lastName);
+    }
+
+    public String toString(){
+        return this.lastName + " " + this.firstName + " " + this.gpa;
     }
 
 }
