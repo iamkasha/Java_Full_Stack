39f7567ee7ac08a8df1b5b1eb20bfd12a37b7309
udaykumarkasha <udayrk19@gmail.com>
2023-06-13 13:33:17 -0500

    exceptions

---
+++ src/Exceptions/InvalidStrToIntException.java
@@ -0,0 +1,31 @@
+package Exceptions;
+
+public class InvalidStrToIntException extends RuntimeException{
+
+   public  InvalidStrToIntException(Exception e){
+       System.out.println(e);
+   }
+
+}
+
+class Main {
+
+    static void toInt(String s) throws InvalidStrToIntException {
+        try
+        {
+           int s1= Integer.parseInt(s);
+            System.out.println(s1);
+        }
+        catch(NumberFormatException e) {
+            throw new InvalidStrToIntException(e);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        toInt("123");
+        toInt("test");
+
+    }
+}
